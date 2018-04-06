5a87dd285272e5e5b78ed313167160945556819b
Mohammed Arfan <35273346+mdarfan@users.noreply.github.com>
2018-04-06 23:31:21 +0530

    Add files via upload

---
+++ bt1ch/bt1ch.ino
@@ -0,0 +1,34 @@
+int led=2;
+
+
+
+char val;
+void setup() {
+  Serial.begin(9600);
+  pinMode(led,OUTPUT);
+
+  
+
+}
+
+void loop() {
+  if(Serial.available()>0)
+  {val=Serial.read();
+  Serial.println(val);
+  if(val=='A')
+  {
+    digitalWrite(led,HIGH);
+    Serial.println("on");
+
+  }
+  if(val=='B')
+  {
+    digitalWrite(led,LOW);
+    Serial.println("off");
+
+ 
+  }
+
+ 
+  }
+}
